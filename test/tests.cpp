46693a3708da11ba64b2b4dafe9fbb9e5794d5ea
Ubuntu <suhasghorp@my-azure-vm.hvm3ho1r4c0uxpu23nzycwe35g.bx.internal.cloudapp.net>
2021-12-15 16:18:10 +0000

    adding cpplib

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch.hpp>
 
+#include "../src/lib/cpplib.h"
+
 unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
 {
   return number <= 1 ? number : Factorial(number - 1) * number;
@@ -11,4 +13,38 @@ TEST_CASE("Factorials are computed", "[factorial]")
   REQUIRE(Factorial(2) == 2);
   REQUIRE(Factorial(3) == 6);
   REQUIRE(Factorial(10) == 3628800);
+}
+
+TEST_CASE("FindMax", "[cpplib]") {
+  CPPLib cpplib;
+
+  SECTION("Should find max of consecutive numbers") {
+    std::vector<int> inputs = {1, 2, 3, 4};
+    REQUIRE(cpplib.FindMax(inputs) == 4);
+  }
+
+  SECTION("Should find max of non consecutive numbers") {
+    std::vector<int> inputs = {1, 7, 3, 4};
+    REQUIRE(cpplib.FindMax(inputs) == 7);
+  }
+
+  SECTION("Should find max with negative numbers") {
+    std::vector<int> inputs = {-1, -7, -3, -4};
+    REQUIRE(cpplib.FindMax(inputs) == -1);
+  }
+
+  SECTION("Should find max of all equal values") {
+    std::vector<int> inputs = {-1, -1, -1, -1};
+    REQUIRE(cpplib.FindMax(inputs) == -1);
+  }
+
+  SECTION("Should find return min for empty vector") {
+    std::vector<int> inputs = {};
+    REQUIRE(cpplib.FindMax(inputs) == std::numeric_limits<int>::min());
+  }
+
+  SECTION("Should find return min for empty vector") {
+    std::vector<int> inputs = {1000};
+    REQUIRE(cpplib.FindMax(inputs) == 1000);
+  }
 }
