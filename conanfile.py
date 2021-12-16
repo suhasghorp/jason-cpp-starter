36b880a1704ae9b0c20f169149ab6d10e9b3a291
Ubuntu <suhasghorp@my-azure-vm.hvm3ho1r4c0uxpu23nzycwe35g.bx.internal.cloudapp.net>
2021-12-16 00:58:18 +0000

    downgrading tbb to 2020.0

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class CppStarterProject(ConanFile):
         "docopt.cpp/0.6.2",
         "fmt/8.0.1",
         "spdlog/1.9.2",
-        "tbb/2020.3"
+        "tbb/2020.0"
     )
     generators = "cmake", "gcc", "txt", "cmake_find_package"
 
