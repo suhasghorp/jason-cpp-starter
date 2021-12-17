31ea0d47cd0e1bb8a295c1322ff248921e863e1a
Ubuntu <suhasghorp@my-azure-vm.hvm3ho1r4c0uxpu23nzycwe35g.bx.internal.cloudapp.net>
2021-12-17 14:15:32 +0000

    adding TBB parallel_for test

--- conanfile.py
+++ conanfile.py
@@ -9,9 +9,9 @@ class CppStarterProject(ConanFile):
         "cpp_starter_use_sdl": ["ON", "OFF", ""]
     }
     #settings = "cppstd","os"
-    settings = "compiler": {"gcc": {"cppstd": [17]},
+    settings = {"compiler": {"gcc": {"cppstd": [17]},
                  "Visual Studio": {"cppstd": [17]},
-                 "Clang":{"cppstd":[17]}}, "os"
+                 "Clang":{"cppstd":[17]}}, "os"}
     name = "CppStarterProject"
     version = "0.1"
     '''requires = (
